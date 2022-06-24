23c093da23b8ca6c8a794ca46046144f3aad3575
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-06-25 00:47:42 +0600

    created fields in the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -4,8 +4,16 @@ from mptt.admin import MPTTModelAdmin
 from . import models
 
 
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "author", "create_at", "id"]
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["name", "prep_time", "cook_time", "post"]
+
+
 admin.site.register(models.Category, MPTTModelAdmin)
 admin.site.register(models.Tag)
-admin.site.register(models.Post)
-admin.site.register(models.Recipe)
 admin.site.register(models.Comment)
