fe6a60421dc7041451cc44c4b31ce875adf15124
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-06-28 20:04:26 +0600

    added views

--- blog/urls.py
+++ blog/urls.py
@@ -1,2 +1,11 @@
 from django.urls import path
-# from
+from . import views
+
+
+"""
+slug -> это тоже самое что charfild, но для урлов(ссылок)
+"""
+urlpatterns = [
+    path('<slug:slug>', views.PostListView.as_view(), name="post_list"),
+    path('', views.home),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from blog.models import Post
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs.get("slug"))
 
 
 def home(request):
