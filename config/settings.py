672dc2f242e9bdf5a039ca9b6d82ddd27c929bcf
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-07-04 00:37:23 +0600

    added django-debug-toolbar library

--- config/settings.py
+++ config/settings.py
@@ -38,14 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'debug_toolbar',
     'mptt',
     'ckeditor',
 
     'blog',
+    'contact',
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,3 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
+
+
+INTERNAL_IPS = '127.0.0.1'
--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -6,6 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
     path('', include('blog.urls')),
 ]
 
